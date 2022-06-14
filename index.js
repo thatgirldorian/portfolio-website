29620e3ae7e53b127976f1469b60071a2776d168
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-06-14 23:32:13 +0400

    FIXED STICKY HEADER, YAAAY!

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-//make the header sticky
-window.onscroll = function() {makeSticky()};
+// //make the header sticky
+// window.onscroll = function() {makeSticky()};
 
-// Get the header
-let header = document.querySelector(".navbar");
+// // Get the header
+// let header = document.querySelector(".navbar");
 
-// Get the offset position of the navbar
-let sticky = header.offsetTop;
+// // Get the offset position of the navbar
+// let sticky = header.offsetTop;
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function makeSticky() {
-    if (window.pageYOffset > sticky) {
-    header.classList.add("sticky-header");
-    } else {
-    header.classList.remove("sticky-header");
-    }
-}
+// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function makeSticky() {
+//     if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky-header");
+//     } else {
+//     header.classList.remove("sticky-header");
+//     }
+// }
